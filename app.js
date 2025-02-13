fd6e713814569f3a87af2f70d92125c89393bb33
DanrevLazo <161433786+Danrev29@users.noreply.github.com>
2025-02-13 17:42:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const text = document.querySelector("#text");
 const vid = document.querySelector("video");
 let count = 2;
 
+const gifs = [
+  "../resources/cat-heart.gif",
+  "../resources/rusure.gif",
+  "../resources/3shocked-1.gif",
+  "../resources/4.crying.gif",
+  "../resources/5.crying.gif",
+  "../resources/idc.gif"
+];
+
 // to load the gifs faster
 gifs.forEach(gifSrc => {
   const img = new Image();
